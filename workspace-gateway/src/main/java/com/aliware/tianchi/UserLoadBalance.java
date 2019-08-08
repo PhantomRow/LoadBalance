02c454a0d0cafe3298e999f0bb61c02868533bba
yuuko ichihara <1174187169@qq.com>
2019-08-08 22:43:27 +0800

    更换迭代函数为二次反比

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -80,7 +80,9 @@ public class UserLoadBalance implements LoadBalance {
                     }
 
                     // 计算权重
-                    weight = weight * (500 / clientTimeAvgSpendCurr);
+                    weight = weight * (500 / clientTimeAvgSpendCurr * clientTimeAvgSpendCurr);
+
+                   // weight = weight * (500 / clientTimeAvgSpendCurr);
                     serviceWeight[index] = weight;
                     totalWeight = totalWeight + weight;
                 }
